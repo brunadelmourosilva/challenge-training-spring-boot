deb86638b6822779a49134dcf7ad024e9f7d5a71
Bruna Delmouro <Bruna.Delmouro-ext@ab-Inbev.com>
2022-07-23 23:18:38 -0300

    Adicionando dados para o curso

--- src/main/java/com/brunadelmouro/challengespring/ChallengeWithSpringBootApplication.java
+++ src/main/java/com/brunadelmouro/challengespring/ChallengeWithSpringBootApplication.java
@@ -1,15 +1,35 @@
 package com.brunadelmouro.challengespring;
 
+import com.brunadelmouro.challengespring.models.Curso;
+import com.brunadelmouro.challengespring.repositories.CursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 @EnableJpaAuditing
-public class ChallengeWithSpringBootApplication {
+public class ChallengeWithSpringBootApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeWithSpringBootApplication.class, args);
 	}
 
+	@Autowired
+	CursoRepository cursoRepository;
+
+	@Override
+	public void run(final String... args) throws Exception {
+
+		Curso curso1 = new Curso(null, "Sistemas de Informação", "SIN");
+		Curso curso2 = new Curso(null, "Engenharia da Computação", "ECO");
+		Curso curso3 = new Curso(null, "Matemática Bacharelado", "MAT");
+
+		//TODO relacionamento com as tabelas
+
+		cursoRepository.saveAll(Arrays.asList(curso1, curso2, curso3));
+	}
 }
---
+++ src/main/java/com/brunadelmouro/challengespring/repositories/CursoRepository.java
@@ -0,0 +1,9 @@
+package com.brunadelmouro.challengespring.repositories;
+
+import com.brunadelmouro.challengespring.models.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Integer> {
+}
